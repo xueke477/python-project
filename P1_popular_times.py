199f7f9e670f2f2141f73146aa210c33fd84f2aa
Ke_Xue <xueke.kent@gmail.com>
2020-10-06 15:18:15 -0400

    Finish P1

--- P1_popular_times.py
+++ P1_popular_times.py
@@ -7,16 +7,17 @@ df = pd.read_csv(filename)
 
 # convert the Start Time column to datetime
 df['Start Time'] = pd.to_datetime(df['Start Time'])
-print(type(df['Start Time'][:2]))
-print(df['Start Time'][:2])
-print([x.hour for x in df['Start Time'][:2]])
-# print(df['Start Time'][:2].hour)
-# print(type(df['Start Time'][:2].hour))
 
 # extract hour from the Start Time column to create an hour column
-# df['hour'] =
+df['hour'] = [x.hour for x in df['Start Time']]
+# test = df.iloc[:][:5]
+# print(type(test['hour']))
+# print(test['hour'].values)
+# print(test['hour'].index)
+# print(test['hour'].mode()[0])
+# print(test.groupby('hour')['hour'].count().index)
 
 # find the most common hour (from 0 to 23)
-# popular_hour =
+popular_hour = df.groupby('hour')['hour'].count().idxmax()
 
-# print('Most Frequent Start Hour:', popular_hour)
+print('Most Frequent Start Hour:', popular_hour)
