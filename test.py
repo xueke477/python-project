2a895e32da339a71f514ee95e464ed86a5cc317c
Ke_Xue <xueke.kent@gmail.com>
2020-10-08 01:04:18 -0400

    Test Git core.editor

--- test.py
+++ test.py
@@ -1,9 +1,10 @@
 import pandas as pd
 
-df = pd.read_csv('chicago.csv')
+test = pd.read_csv('chicago.csv')
 
-# df = test[:][:10]
+df = test[:][:10]
 
+"""
 df['Start Time'] = pd.to_datetime(df['Start Time'])
 df['month'] = df['Start Time'].dt.month_name()
 df['day_of_week'] = df['Start Time'].dt.day_name()
@@ -11,7 +12,7 @@ df['hour'] = df['Start Time'].dt.hour
 
 row_index = df['Birth Year'].idxmin()
 print(df.iloc[row_index, :-3])
-
+"""
 
 # print(df.columns)
 # print(type(df.iloc[0, :-3]))
